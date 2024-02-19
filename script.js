2d0fb687d97a14f3063e29fefbe8c681143da84e
Tirtha Sharma <tirthagenze121@gmail.com>
2024-02-19 19:20:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
+const todayDate = document.querySelector(".today");
+todayDate.value = new Date().toLocaleDateString();
+
+if (result.title === "") {
+  result.innerHTML =
+    `Age : <span>${0}</span>` +
+    ` Month : <span>${0}</span>` +
+    ` Year : <span>${0}</span>`;
+}
+
+function calculateAge() {
+  let birthDate = new Date(userInput.value);
+  let d1 = birthDate.getDate();
+  let m1 = birthDate.getMonth() + 1;
+  let y1 = birthDate.getFullYear();
+
+  let today = new Date();
+
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
+
+  let d3, m3, y3;
+
+  y3 = y2 - y1;
+
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = getDaysInMonth(y1, m1) + d2 - d1;
+  }
+
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
+
+  if (y3 !== NaN && m3 !== NaN && d3 !== NaN && result.title === "") {
+    result.innerHTML =
+      `Age : <span>${0}</span>` +
+      ` Month : <span>${0}</span>` +
+      ` Year : <span>${0}</span>`;
+  } else {
+    result.innerHTML = `You are <span>${"0" + y3}</span> years, <span>${
+      "0" + m3
+    } </span> months, and <span>${d3} </span> days old`;
+  }
+}
+
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
+
+function clearData() {
+  userInput.value = "";
+  result.innerHTML =
+    `Age : <span>${0}</span>` +
+    ` Month : <span>${0}</span>` +
+    ` Year : <span>${0}</span>`;
+}
